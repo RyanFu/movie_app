2e5ddc096375aa271d8c1442c99dc9ea00407a47
Steven Ko <chunyuko85@gmaill.com>
2012-08-17 21:39:09 +0800

    change gcm app_name

--- config/initializers/gcm_on_rails.rb
+++ config/initializers/gcm_on_rails.rb
@@ -1,4 +1,4 @@
 configatron.gcm_on_rails.api_url = 'https://android.googleapis.com/gcm/send'
 configatron.gcm_on_rails.api_key = 'AIzaSyC4TmOIcgIf8QyHnrHr39SeXthK7VX58t0'
-configatron.gcm_on_rails.app_name = 'com.google.android.gcm.demo'
+configatron.gcm_on_rails.app_name = 'com.jumplife.moviediary'
 configatron.gcm_on_rails.delivery_format = 'json'
