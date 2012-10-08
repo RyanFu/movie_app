eeb07e1ac7681773159d46f2684c01b19e581a64
Steven Ko <chunyuko85@gmaill.com>
2012-10-08 14:45:24 +0800

    asset issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,6 +47,16 @@ namespace :deploy do
   task :update_crontab, :roles => :db do
     run "cd #{release_path} && bundle exec whenever --update-crontab #{application}"
   end
+
+  namespace :assets do
+    desc "Precompile assets on local machine and upload them to the server."
+    task :precompile, roles: :web, except: {no_release: true} do
+      run_locally "bundle exec rake assets:precompile"
+      find_servers_for_task(current_task).each do |server|
+        run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
+      end
+    end
+  end
 end
 
 
