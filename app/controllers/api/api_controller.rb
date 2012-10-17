ea0a5ae3c588d4755872539966c887faedcbc72e
Steven Ko <chunyuko85@gmaill.com>
2012-10-17 22:00:57 +0800

    promotion test

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class Api::ApiController  < ActionController::Base
   respond_to :json
   before_filter :check_format_json
@@ -10,9 +11,9 @@ class Api::ApiController  < ActionController::Base
   end
 
   def promotion
-    promotion = {:picture_link => nil, 
-                 :link => nil,
-                 :tilte => nil,
+    promotion = {:picture_link => "https://lh5.ggpht.com/glegjFK0dms70GmBldnJ9MgB3MG4fm9_rd2J8P578-g3kMtMxEayJLn4MWm7wwBqVNw=w124", 
+                 :link => "https://play.google.com/store/apps/details?id=com.jumplife.moviediary",
+                 :tilte => "電影櫃",
                  :description => nil
     }
 
