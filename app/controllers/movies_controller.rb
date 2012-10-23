df02ea4f158fa00d35dd369bf3b4f0014fe17da1
Steven Ko <chunyuko85@gmaill.com>
2012-10-23 11:28:05 +0800

    movie index first step finish

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,9 +6,30 @@ class MoviesController < ApplicationController
 
   def index
     @hot_movies = MovieBoxOfficeShip.hot_movies
+    @hot_movies_array = separate_array_by_num(@hot_movies,4)
     @first_round_movies = Movie.first_round.order("release_date DESC")
     @second_round_movies = Movie.second_round.order("release_date DESC")
     @comming_movies = Movie.comming
     @this_week_movies = Movie.this_week
+  end
+
+
+  private
+
+  def separate_array_by_num (array_val,num)
+    array_size = array_val.length / num +1;
+    return_array = Array.new(array_size)
+    i = 0
+    array = Array.new
+    array_val.each_with_index {|element, index|
+        array << element
+        i += 1
+        if i%4 == 0
+          return_array[i/num-1] = array
+          array = Array.new
+        end
+    }
+    return_array[array_size-1] = array
+    return_array
   end
 end
