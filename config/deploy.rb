6e1615eadcee33f0b1f2e1089cb5f5cf8716181c
Steven Ko <chunyuko85@gmaill.com>
2012-10-25 19:13:41 +0800

    change deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,8 @@ set :default_environment, {
     'PATH' => "/opt/ruby/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
 }
 
+set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
+
 
 namespace :deploy do
   
@@ -49,20 +51,45 @@ namespace :deploy do
   end
 
   namespace :assets do
-    desc "Precompile assets on local machine and upload them to the server."
-    task :precompile, roles: :web, except: {no_release: true} do
-      run_locally "bundle exec rake assets:precompile"
-      find_servers_for_task(current_task).each do |server|
-        run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
+
+    desc <<-DESC
+      Run the asset precompilation rake task. You can specify the full path \
+      to the rake executable by setting the rake variable. You can also \
+      specify additional environment variables to pass to rake via the \
+      asset_env variable. The defaults are:
+
+        set :rake,      "rake"
+        set :rails_env, "production"
+        set :asset_env, "RAILS_GROUPS=assets"
+        set :assets_dependencies, fetch(:assets_dependencies) + %w(config/locales/js)
+    DESC
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
       end
     end
+
   end
+  
+  # precompile asset local!!
+  # namespace :assets do
+  #   desc "Precompile assets on local machine and upload them to the server."
+  #   task :precompile, roles: :web, except: {no_release: true} do
+  #     run_locally "bundle exec rake assets:precompile"
+  #     find_servers_for_task(current_task).each do |server|
+  #       run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
+  #     end
+  #   end
+  # end
 end
 
 
 after "deploy:stop",    "delayed_job:stop"
 after "deploy:start",   "delayed_job:start"
 after "deploy:restart", "delayed_job:restart"
-after "deploy:update_code", "deploy:copy_config_files" # 如果將database.yml放在shared下，請打開
+before "deploy:assets:precompile", "deploy:copy_config_files" # 如果將database.yml放在shared下，請打開
 after "deploy:create_symlink", "deploy:update_crontab"
 # after "deploy:finalize_update", "deploy:update_symlink" # 如果有實作使用者上傳檔案到public/system，請打開
