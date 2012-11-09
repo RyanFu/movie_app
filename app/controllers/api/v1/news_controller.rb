6bb7f33e5e27ab137d09d3c2f827acc32805cc1a
Steven Ko <chunyuko85@gmaill.com>
2012-11-09 16:27:35 +0800

    news paginate order

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::NewsController < ApplicationController
   def index
 
     if params[:page]
-      @news = News.paginate(:page => params[:page], :per_page => 20)
+      @news = News.by_id_desc.paginate(:page => params[:page], :per_page => 20)
     else
       @news = News.by_id_desc
     end
