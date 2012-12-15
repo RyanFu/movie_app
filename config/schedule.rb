1f5cbf038f53f29a0f77c26d0d61c2597444e800
Steven Ko <chunyuko85@gmaill.com>
2012-12-15 09:18:27 +0800

    change schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -45,7 +45,7 @@ every :saturday, :at => '07:30am' do
   rake 'crawl:crawl_yahoo_on_view',:output => {:error => 'log/error.log', :standard => 'log/cron.log'}
 end
 
-every :saturday, :at => '09:00am' do
+every :saturday, :at => '09:30am' do
   rake 'crawl:parse_movie_time_and_theater_ship',:output => {:error => 'log/error.log', :standard => 'log/cron.log'}
 end
 
