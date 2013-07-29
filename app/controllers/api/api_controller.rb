5360dabf90aec355abe64ea0aa04e88514df5ac9
RayWoo <raywu07@gmail.com>
2013-07-29 11:42:27 +0800

    modify promotion

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -11,15 +11,26 @@ class Api::ApiController  < ActionController::Base
   end
 
   def promotion
-    promotion = {:picture_link => "ttp://pic.pimg.tw/jumplives/1367922942-4188405356.jpg?v=1367922978", 
-                 :link => "https://play.google.com/store/apps/details?id=com.jumplife.movienews&referrer=utm_source%3Dmovie_diary_inapp%26utm_campaign%3Dmovie_diary_inapp",
-                 :tilte => "電影窩",
-                 :description => "電影經典名句, 深度影評, 明星趣聞 - 趕快加入窩在電影窩App的行列吧!",
-                 :version => 2
+    promotion = {:picture_link => "http://ext.pimg.tw/jumplives/1352973893-4060856514.png?v=1352973903", 
+                 :link => "http://goo.gl/a4gOqO",
+                 :tilte => "幫電影櫃評分",
+                 :description => "歡迎到 Google Play 給電影櫃中肯的建議與評價, 謝謝！",
+                 :version => 3
     }
 
     render :json => promotion.to_json
   end
+
+#  def promotion
+#    promotion = {:picture_link => "ttp://pic.pimg.tw/jumplives/1367922942-4188405356.jpg?v=1367922978", 
+#                 :link => "https://play.google.com/store/apps/details?id=com.jumplife.movienews&referrer=utm_source%3Dmovie_diary_inapp%26utm_campaign%3Dmovie_diary_inapp",
+#                 :tilte => "電影窩",
+#                 :description => "電影經典名句, 深度影評, 明星趣聞 - 趕快加入窩在電影窩App的行列吧!",
+#                 :version => 2
+#    }
+
+#    render :json => promotion.to_json
+#  end
 
   #def movieinfo_promotion
   #  promotion = {:picture_link => "http://ext.pimg.tw/jumplives/1352973893-4060856514.png?v=1352973903", 
